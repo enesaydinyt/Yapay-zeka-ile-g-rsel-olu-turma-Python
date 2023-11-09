9121b396bc6f52b624bff3bfbc1ff56ecb8d43f2
Enes <42686313+enesaydinyt@users.noreply.github.com>
2023-11-09 16:25:41 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,18 @@
 import openai
 from googletrans import Translator
 import requests
-from PIL import Image
+from PIL import Image, ImageTk
+
 from io import BytesIO
 import tkinter as tk
 from tkinter import PhotoImage ,simpledialog, messagebox
 
-openai.api_key = 'key' # Openai api key
+openai.api_key = '#########################' # api key
 translator = Translator()
-
+photo_label = None
+text_label = None
 def draw():
+  global photo_label, text_label
   try:
     user_input = entry.get()
     example = translator.translate(user_input, dest="en")
@@ -19,17 +22,24 @@ def draw():
       n=1,
       size="1024x1024"
     )
-    label = tk.Label(root, text=("İstenen çizim : ",user_input, " İngizilcesi : ",example.text))
-    label.pack()
+    # Eğer metin etiketi önceden oluşturulmuşsa, sadece metni güncelle
+    if text_label is not None:
+      text_label.configure(text="İstenen çizim: " + user_input + "\nİngilizcesi: " + example.text )
+    else:
+      text_label = tk.Label(root, text="İstenen çizim: " + user_input + "\nİngilizcesi: " + example.text)
+      text_label.pack()
+
     image_url = response['data'][0]['url']
     response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
     img.save('outimg.png')
-    photo = PhotoImage(file="outimg.png")
-    label = tk.Label(root, image=photo)
-
-    label.image = photo
-    label.pack()
+    photo = ImageTk.PhotoImage(img)
+    if photo_label is not None:
+      photo_label.configure(image=photo)
+    else:
+      photo_label = tk.Label(root, image=photo)
+      photo_label.pack()
+    photo_label.image = photo
 
 
     root.mainloop()
@@ -45,9 +55,9 @@ label.pack()
 entry = tk.Entry(root)
 entry.pack()
 
-button = tk.Button(root, text="Çiz", command=draw , width=10, height=1)
+button = tk.Button(root, text="Çiz", command=draw, width=10, height=1)
 button.pack()
-button = tk.Button(root, text="Çıkış", command=root.quit, width=10, height=1)
+button = tk.Button(root, text="Çıkış", command=root.destroy, width=10, height=1)
 button.pack()
 
 root.mainloop()
